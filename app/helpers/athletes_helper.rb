7d3c6d21e0844cce7566029826806ec3ea591e47
Anand Tripathi <anand.tripathi@clariontechnologies.co.in>
2014-07-08 20:36:26 +0530

    Used helper method with athlete views

--- app/helpers/athletes_helper.rb
+++ app/helpers/athletes_helper.rb
@@ -1,2 +1,11 @@
 module AthletesHelper
+  def td_color_depend_on_city(city)
+    if city == "khajuraho"
+      "red"
+    elsif city == "satna"
+      "grey"
+    else
+      "Lavender"
+    end
+  end
 end
