464d4fafba4f4fcc3b1d7b1da7c4d58cf35af612
Anand Tripathi <anand.tripathi@clariontechnologies.co.in>
2014-07-22 11:01:29 +0530

    Added custom logger within rake task

---
+++ lib/custom_logger.rb
@@ -0,0 +1,9 @@
+class CustomLogger < Logger
+  def format_message(severity, timestamp, progname, msg)
+    "#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
+  end
+end
+ 
+logfile = File.open("#{Rails.root}/log/custom.log", 'a') # create log file
+logfile.sync = true # automatically flushes data to file
+CUSTOM_LOGGER = CustomLogger.new(logfile) # constant accessible
